5ba605fcd0431e2b51beed1e999d2f9fb1873306
arashaltafi <altafi.a@nahad.ir>
2024-04-23 12:22:47 +0330

    my route

--- app/api/route.ts
+++ app/api/route.ts
@@ -21,3 +21,22 @@ export async function PATCH() {
 export async function UPDATE() {
     return Response.json({ message: 'Api Route Not Found!' }, { status: 200 })
 }
+
+//// register ->
+// get username, password, name, family
+// create hash password
+// generate token and save to cookie
+// save (username, hashPass, token, userAgent)
+
+//// login ->
+// get username, password and userAgent from header
+// query to get hashPassword with username
+// check password is valid with hashPassword
+// generate token and save to cookie
+// update (token, userAgent)
+
+//// other api ->
+// token and userAgent in header
+// check token is valid
+// if = check userAgent and token is existed -> continue
+// else = 401
