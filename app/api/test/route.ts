f6c573a6588155c75e13b5b789cd76985894585a
arashaltafi <altafi.a@nahad.ir>
2024-04-23 12:05:09 +0330

    token jwt

---
+++ app/api/test/route.ts
@@ -0,0 +1,76 @@
+import { hashPassword, comparePassword, generateToken, verifyToken, decodeToken } from "@/utils/Auth"
+import { NextRequest } from 'next/server'
+
+export async function GET(req: NextRequest) {
+    try {
+        const email = req.nextUrl.searchParams.get('email');
+        const password = req.nextUrl.searchParams.get('password');
+        if (!password || !email) {
+            return Response.json(
+                { message: 'Please Enter email and password in Query Params' },
+                { status: 400 }
+            )
+        }
+        const hashedPassword = await hashPassword(password)
+        const token = await generateToken({
+            email: email
+        })
+
+        return Response.json(
+            {
+                hashedPassword,
+                password,
+                token
+            },
+            { status: 200 }
+        )
+    } catch (error: any) {
+        return Response.json(
+            { message: "Internal Server Error", error: error.message },
+            { status: 500 })
+    }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const { password, hashedPassword } = await req.json()
+        const token = req.headers.get('Authorization')
+
+        if (!password || !hashedPassword) {
+            return Response.json(
+                { message: 'Please Enter password and hashedPassword in Query Params' },
+                { status: 400 }
+            )
+        }
+        if (!token) {
+            return Response.json(
+                { message: 'Please Enter token in header with Authorization' },
+                { status: 400 }
+            )
+        }
+
+        const isSuccessPass = await comparePassword(password, hashedPassword)
+        const isVerifyToken = await verifyToken(token)
+
+        if (isVerifyToken === false) {
+            return Response.json(
+                { message: "Token is inValid" },
+                { status: 401 }) 
+        }
+
+        const { email } = await decodeToken(token)
+
+        return Response.json(
+            {
+                isSuccessPass,
+                isVerifyToken,
+                decodedToken: email
+            },
+            { status: 200 }
+        )
+    } catch (error: any) {
+        return Response.json(
+            { message: "Internal Server Error", error: error.message },
+            { status: 500 })
+    }
+}
--- utils/Auth.ts
+++ utils/Auth.ts
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const appSecret = "2fs7ej7fjafekawfhef92h2389fh239hf"
 
 const hashPassword = async (password: string) => {
     const salt = await bcrypt.genSalt(10);
@@ -10,6 +12,26 @@ const comparePassword = async (password: string, hashedPassword: string) => {
     return await bcrypt.compare(password, hashedPassword);
 }
 
+const generateToken = (data: any) => {
+    return jwt.sign(data, appSecret, {
+        expiresIn: '1m',
+        algorithm: 'HS256'
+    })
+}
+
+const verifyToken = (token: string) => {
+    try {
+        jwt.verify(token, appSecret)
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+const decodeToken = (token: string) => {
+    return jwt.decode(token, appSecret)
+}
+
 export {
-    hashPassword, comparePassword
+    hashPassword, comparePassword, generateToken, verifyToken, decodeToken
 }
