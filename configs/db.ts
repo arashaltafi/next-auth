8e05f4699cb30100612d5b21669546e39743f95a
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-24 01:35:10 +0430

    db connection fixed

--- configs/db.ts
+++ configs/db.ts
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectToDB = async () => {
+    const url = "mongodb://root:zjDnLGwajh1hzVSL4CrnYKwn@makalu.liara.cloud:34721/my-app?authSource=admin"
     try {
-        if (mongoose.connection.readyState === 1) {
-            return true
-        } else {
-            const db = await mongoose.connect("mongodb://root:4bcDTWAyN7WfPUBIlinc6LlR@makalu.liara.cloud:30837/my-app?authSource=admin", {
-                useNewUrlParser: true,
-            });
-            console.log('Connected to MongoDB');
-            return db.connection;
-        }
+        await mongoose.connect(url);
+        return mongoose.connection;
     } catch (error) {
         console.error('DB connection error:', error);
     }
