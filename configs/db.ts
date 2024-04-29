db531616a71dcc96160e697eec2e98c2616cc253
arashaltafi <altafi.a@nahad.ir>
2024-04-29 11:19:24 +0330

    check connection for mongoose

--- configs/db.ts
+++ configs/db.ts
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectToDB = async () => {
     const url = "mongodb://root:zjDnLGwajh1hzVSL4CrnYKwn@makalu.liara.cloud:34721/auth?authSource=admin"
     try {
+        if (mongoose.connections[0].readyState) return mongoose.connection[0];
         await mongoose.connect(url);
         return mongoose.connection;
     } catch (error) {
